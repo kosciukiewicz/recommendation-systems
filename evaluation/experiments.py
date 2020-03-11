65774930bf6be6f7f70c4eeac93f866b1726b579
gabirelik <gabriela.relikowska@gmail.com>
2020-03-11 09:08:07 +0100

    hit rate refactor

--- evaluation/experiments.py
+++ evaluation/experiments.py
@@ -3,6 +3,7 @@ from content_based_recomendation.weigted_rating_cbr import WeightedRatingCbr
 from settings import PATH_TO_DATA
 from content_based_recomendation.scripts.movie_lens_features_extractor import FeaturesExtractor
 from collaborative_filtering.memory_based_collaborative_filtering import MemoryBasedCollaborativeFiltering
+from collaborative_filtering.svd_collaborative_filtering import SVDCollaborativeFiltering
 import pandas as pd
 import os
 
@@ -19,15 +20,20 @@ def main():
 
     movie_mapping = dict(zip(data['id'].tolist(), data.index.astype(int)))
 
+    user_ids = ratings[user_column].unique()
+    movie_ids = ratings[item_column].unique()
+
     hit_rate_ns = [30]
-    methods = [MemoryBasedCollaborativeFiltering(ratings[user_column].unique(),
-                                                 ratings[item_column].unique()),
-               WeightedRatingCbr(data['combined'], movie_mapping)]
+    methods = [MemoryBasedCollaborativeFiltering(user_ids, movie_ids),
+               WeightedRatingCbr(data['combined'], movie_mapping),
+               SVDCollaborativeFiltering(user_ids, movie_ids)]
 
     for n in hit_rate_ns:
         hit_rate = HitRate(n)
         for method in methods:
-            hit_rate.evaluate(method, ratings.values[:, :3])
+            print(type(method))
+            result = hit_rate.evaluate(method, ratings.values[:, :3])
+            print(f'Final result: {result}')
 
 
 if __name__ == '__main__':
