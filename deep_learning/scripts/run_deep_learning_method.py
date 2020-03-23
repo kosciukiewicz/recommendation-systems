12f5d512a77d025dc7c785e3ba541e0252db45fa
Witold <kosciukiewicz.w@gmail.com>
2020-03-23 15:04:08 +0100

    Deep learning code refecatored

--- deep_learning/scripts/run_deep_learning_method.py
+++ deep_learning/scripts/run_deep_learning_method.py
@@ -1,100 +1,75 @@
 import pandas as pd
-import numpy as np
 import os
+import deep_learning.utils as deep_learning_utils
+import pickle as plk
 
 from settings import PATH_TO_DATA, PATH_TO_PROJECT
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-from utils.features_extraction.movie_lens_features_extractor import FeaturesExtractor
 from utils.id_indexer import Indexer
 from utils.evaluation.test_train_split import user_leave_on_out
 
 from deep_learning.movie_features_deep_learning_method import MovieFeaturesDeepLearningMethod
+from collaborative_filtering.neural_collaborative_filtering import NeuralCollaborativeFiltering
 
 user_column = 'userId'
 item_column = 'movieId'
 rating_column = 'rating'
 
 
-def get_movie_id_to_feature_mapping(movies_metadata_df):
-    mapping = {}
-    for i, row in movies_metadata_df.iterrows():
-        features = {
-            "title": row["title"],
-            "id": row["id"],
-        }
+def get_movies_model_features(all_users_ratings_df, train_ratings_df, movies_feature_data_path, saved_movies_features):
+    movies_data_df, movies_features = deep_learning_utils.get_movies_features(movies_feature_data_path,
+                                                                              saved_movies_features)
+    movies_data_df.to_csv(f"{PATH_TO_DATA}/generated/movies_data_df.csv")
+    indexer = Indexer(user_ids=all_users_ratings_df[user_column].unique(), movies_ids=movies_data_df['id'].unique())
 
-        mapping[int(row['id'])] = features
+    user_features = deep_learning_utils.get_weighted_movies_user_features(train_ratings_df, indexer, movies_features)
 
-    return mapping
+    return movies_features, user_features, indexer
 
 
-def get_weighted_movies_user_features(user_ratings_df, indexer, movies_features):
-    user_features = []
-
-    for user_internal_id, user_id in indexer.internal_id_to_user_id_dict.items():
-        user_ratings = user_ratings_df[user_ratings_df[user_column] == user_id][[item_column, rating_column]].values
-        user_rated_movies_id = [indexer.get_movie_internal_id(i) for i in user_ratings[:, 0].astype(int)]
-        user_ratings = np.expand_dims(user_ratings[:, 1] / 5.0, axis=1)
-        user_rated_movies_features = movies_features[user_rated_movies_id, :]
-        user_movies_features = np.sum(np.multiply(user_ratings, user_rated_movies_features), axis=0)
-        user_features.append(user_movies_features)
-
-    return np.array(user_features)
-
-
-def map_df_to_model_train_input(data_df, movies_features, user_features, indexer):
+def map_df_to_model_input(data_df):
     data = data_df[[user_column, item_column, rating_column]].values
-    return [(user_features[indexer.get_user_internal_id(r[0])],
-             movies_features[indexer.get_movie_internal_id(r[1])],
-             r[2]) for r in data]
-
-
-def map_df_to_test_input(data_df, movies_features, user_features, indexer):
-    data = data_df[[user_column, item_column, rating_column]].values
-    return [(indexer.get_user_internal_id(r[0]),
-             indexer.get_movie_internal_id(r[1]),
+    return [(int(r[0]),
+             int(r[1]),
              r[2]) for r in data]
 
 
 def main():
-    user_ratings_df = pd.read_csv(f"{PATH_TO_DATA}/raw/the-movies-dataset/ratings_small.csv")
-    movies_metadata = pd.read_csv(f"{PATH_TO_DATA}/raw/the-movies-dataset/movies_metadata_clean.csv")
-    movie_id_features_dict = get_movie_id_to_feature_mapping(movies_metadata)
+    dataset_path = os.path.join(os.sep, PATH_TO_DATA, "raw/the-movies-dataset")
+    path_to_saved_features = os.path.join(os.sep, PATH_TO_DATA, "generated/movies_data_df.csv")
+    user_ratings_df = pd.read_csv(os.path.join(dataset_path, "ratings_small.csv"))
+    movies_metadata = pd.read_csv(os.path.join(dataset_path, "movies_metadata_clean.csv"))
+    movie_id_features_dict = deep_learning_utils.get_movie_id_to_feature_mapping(movies_metadata)
 
     user_ratings_df = user_ratings_df[user_ratings_df[item_column].isin(movie_id_features_dict.keys())]
-
-    # train_ratings_df, test_ratings_df = train_test_split(user_ratings_df, train_size=0.8, shuffle=True,
-    #                                                     random_state=123)
 
     train_ratings_df, test_ratings_df = \
         list(user_leave_on_out(user_ratings_df, timestamp_column='timestamp', make_user_folds=False))[0]
 
-    dataset_path = f'{PATH_TO_DATA}/raw/the-movies-dataset'
-    features_extractor = FeaturesExtractor(dataset_path)
-    movies_data = features_extractor.run()
+    movies_features, user_features, indexer = get_movies_model_features(user_ratings_df, train_ratings_df, dataset_path,
+                                                                        path_to_saved_features)
 
-    cv = CountVectorizer(min_df=10)
-    movies_features = cv.fit_transform(movies_data['keywords']).toarray().astype(float)
-    indexer = Indexer(user_ids=user_ratings_df[user_column].unique(), movies_ids=movies_data['id'])
-    user_features = get_weighted_movies_user_features(train_ratings_df, indexer, movies_features)
+    train_data = map_df_to_model_input(train_ratings_df)
+    test_data = map_df_to_model_input(test_ratings_df)
 
-    train_data = map_df_to_model_train_input(train_ratings_df, movies_features, user_features, indexer)
-    test_data = map_df_to_test_input(test_ratings_df, movies_features, user_features, indexer)
+    #method = NeuralCollaborativeFiltering(indexer=indexer, n_factors=128, model_name="neucf_128_wo_threshold")
 
-    method = MovieFeaturesDeepLearningMethod()
-    # method.fit(train_data, test_data, epochs=20)
-    method.load_model(filepath=os.path.join(PATH_TO_PROJECT, "deep_learning", "saved_models", "model.h5"),
-                      input_size=movies_features.shape[1])
+    method = MovieFeaturesDeepLearningMethod(indexer=indexer, movies_features=movies_features,
+                                             user_features=user_features, model_name="movies_features")
 
-    for user, movie, rating in test_data[:6]:
-        recommendations = method.get_recommendations(user_features[user, :], movies_features, k=10)
+    #method.fit(train_data, test_data, batch_size=50, epochs=50, early_stopping=-1)
 
-        user_rated_movies = user_ratings_df[user_ratings_df[user_column] == indexer.get_user_id(user)] \
-            .sort_values(rating_column, ascending=False)[[item_column]]\
+    method.load_model(filepath=os.path.join(PATH_TO_PROJECT, "deep_learning", "saved_models", "movies_features.h5"))
+
+    for user, movie, rating in test_data[:10]:
+        recommendations = method.get_recommendations(user_features[indexer.get_user_internal_id(user), :],
+                                                     movies_features)
+
+        user_rated_movies = train_ratings_df[train_ratings_df[user_column] == user] \
+            .sort_values(rating_column, ascending=False)[[item_column]] \
             .values.squeeze()
 
-        recommended_movies = [indexer.get_movie_id(movie_internal_id) for movie_internal_id in recommendations]
+        recommended_movies = [indexer.get_movie_id(movie_internal_id) for movie_internal_id in recommendations if
+                              movie_internal_id not in user_rated_movies][:30]
 
         print("Rated movies: ")
         for movie_id in user_rated_movies:
@@ -103,6 +78,9 @@ def main():
         print("Recommended movies: ")
         for movie_id in recommended_movies:
             print(movie_id_features_dict[movie_id])
+
+        print("Test movie rating: ")
+        print(movie_id_features_dict[movie], rating)
 
 
 if __name__ == '__main__':
