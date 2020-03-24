b00df41cc1b9b79f5230fc62ad2da14c1617996a
gabirelik <gabriela.relikowska@gmail.com>
2020-03-24 14:18:29 +0100

    add asociation rules based recommendation

--- association_rules/association_rules_based_recommendation.py
+++ association_rules/association_rules_based_recommendation.py
@@ -4,24 +4,43 @@ from itertools import groupby
 from operator import itemgetter
 from interfaces import RecommendationMethod
 from mlxtend.preprocessing import TransactionEncoder
-from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import apriori, association_rules
+from utils.matrix_generator import RatingsBasedMatrixGenerator
 
 
 class AssociationRulesRecommendation(RecommendationMethod):
 
-    def __init__(self, indexer, min_support):
+    def __init__(self, indexer, min_support, min_confidence):
         self.indexer = indexer
         self.min_support = min_support
+        self.min_confidence = min_confidence
 
         self.frequent_itemsets = None
+        self.rules = None
+        self.users_ratings_matrix = None
 
     def fit(self, ratings):
         ratings = self.map_ratings_to_internal_representation(ratings)
         dataset = self.get_dataset(ratings)
         self.frequent_itemsets = self.calc_frequent_itemset(dataset)
+        self.rules = self.calc_association_rules()
+        self.users_ratings_matrix = RatingsBasedMatrixGenerator(
+            self.indexer).generate_user_matrix(ratings)
 
     def get_recommendations(self, user_id, n):
-        pass
+        user_id = self.indexer.get_user_internal_id(user_id)
+        sorted_rule_ids = self.sort_rules_ids_for_user(user_id)
+        watched_movies = np.nonzero(self.users_ratings_matrix[user_id, :])[0]
+
+        selected_movies = []
+        for id in sorted_rule_ids:
+            consequents = self.select_consequents(id, selected_movies,
+                                                  watched_movies)
+            if len(selected_movies) + len(consequents) >= n:
+                return self.map_final_recommendation(
+                    selected_movies + consequents[:n - len(selected_movies)])
+            else:
+                selected_movies += consequents
 
     def map_ratings_to_internal_representation(self, ratings):
         return [(self.indexer.get_user_internal_id(user_id),
@@ -41,7 +60,31 @@ class AssociationRulesRecommendation(RecommendationMethod):
         frequent_itemsets = apriori(dataset,
                                     min_support=self.min_support,
                                     use_colnames=True)
-        frequent_itemsets = frequent_itemsets[
-            frequent_itemsets['itemsets'].apply(
-                lambda itemset: len(itemset)) > 1]
+        # frequent_itemsets = frequent_itemsets[
+        #     frequent_itemsets['itemsets'].apply(
+        #         lambda itemset: len(itemset)) > 1]
         return frequent_itemsets
+
+    def calc_association_rules(self):
+        return association_rules(self.frequent_itemsets,
+                                 metric="confidence",
+                                 min_threshold=self.min_confidence)
+
+    def sort_rules_ids_for_user(self, user_id):
+        def score_rule(row):
+            return np.mean(
+                [self.users_ratings_matrix[user_id, movie_id]
+                 for movie_id in row['antecedents']]
+            ) * row['lift']
+
+        return np.argsort(-self.rules.apply(score_rule, axis=1).values)
+
+    def select_consequents(self, rule_id, selected_movies, watched_movies):
+        all_consequents = self.rules.iloc[rule_id]['consequents']
+        return [movie_id for movie_id in all_consequents if
+                movie_id not in selected_movies and
+                movie_id not in watched_movies]
+
+    def map_final_recommendation(self, intarnal_movies_ids):
+        return [self.indexer.get_movie_id(internal_id)
+                for internal_id in intarnal_movies_ids]
--- association_rules/scripts/run_association_rules_based_method.py
+++ association_rules/scripts/run_association_rules_based_method.py
@@ -8,7 +8,7 @@ from utils.id_indexer import Indexer
 from utils.features_extraction.movie_lens_features_extractor import \
     FeaturesExtractor
 
-pd.set_option('display.max_rows', 5000)
+pd.set_option('display.max_rows', 30000)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_colwidth', -1)
 
@@ -26,27 +26,41 @@ def replace_existing_movies_ids_with_titles(ids, movies_titles):
     return {id: try_replace_id_with_title(id) for id in ids}
 
 
+def get_frequent_itemset_with_titles(arbr, indexer, dataset_path):
+    features_extractor = FeaturesExtractor(dataset_path)
+    movies_data = features_extractor.load()
+    movies_titles = dict(zip(movies_data['id'], movies_data['title']))
+
+    movies_titles = replace_existing_movies_ids_with_titles(
+        indexer.movie_id_to_internal_id_dict.keys(), movies_titles)
+    frequent_itemsets = arbr.frequent_itemsets['itemsets'].apply(
+        lambda itemset: [movies_titles[indexer.internal_id_to_movie_id_dict[id]] for id in itemset])
+    return frequent_itemsets
+
+
+def find_min_confidence(ratings, indexer, min_support):
+    for min_confidence in [0.6, 0.4, 0.3, 0.2, 0.15, 0.1]:
+        arbr = AssociationRulesRecommendation(indexer, min_support, min_confidence)
+        arbr.fit(ratings)
+        print(arbr.rules.shape)
+
+
 def main():
     dataset_path = os.path.join(PATH_TO_DATA, 'raw/the-movies-dataset')
     ratings = pd.read_csv(
         os.path.join(dataset_path, 'ratings_small.csv')).values[:, :-1]
 
-    features_extractor = FeaturesExtractor(dataset_path)
-    movies_data = features_extractor.load()
-    movies_titles = dict(zip(movies_data['id'], movies_data['title']))
-
     indexer = Indexer(user_ids=np.unique(ratings[:, 0]),
                       movies_ids=np.unique(ratings[:, 1]))
-    movies_titles = replace_existing_movies_ids_with_titles(
-        indexer.movie_id_to_internal_id_dict.keys(), movies_titles)
 
-    arbr = AssociationRulesRecommendation(indexer, .15)
+    arbr = AssociationRulesRecommendation(indexer, .15, .2)
     arbr.fit(ratings)
 
-    frequent_itemsets = arbr.frequent_itemsets['itemsets'].apply(
-        lambda itemset: [movies_titles[indexer.internal_id_to_movie_id_dict[id]] for id in itemset])
+    print(arbr.get_recommendations(1, 5))
 
-    print(frequent_itemsets)
+    # print(len(set(ratings[:, 1])))
+
+
 
 
 if __name__ == '__main__':
